9e248cb0b0b05c445fe52d31d7e18514e5f3fa61
pashkoAntonO <anton.pashko.ol@gmail.com>
2017-09-11 15:24:47 +0300

    start

---
+++ Cat.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Anton
+ * Date: 11.09.2017
+ * Time: 15:23
+ */
+class Cat
+{
+
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anton
+ * Date: 11.09.2017
+ * Time: 15:23
+ */
