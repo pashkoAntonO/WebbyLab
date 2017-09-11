d126a2499c000631bd1ad344beb277ad016358cf
pashkoAntonO <anton.pashko.ol@gmail.com>
2017-09-11 15:30:57 +0300

    add Cat

--- Cat.php
+++ Cat.php
@@ -6,7 +6,18 @@
  * Date: 11.09.2017
  * Time: 15:23
  */
-class Cat
+require_once  'Animal.php';
+
+class Cat extends Animal
 {
+
+    function  __construct($name)
+    {
+        parent::__construct($name);
+    }
+
+    function meow():string {
+        return "Cat {$this->name} is saying meow";
+    }
 
 }
